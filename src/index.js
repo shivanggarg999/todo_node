7eed6437b675558f3647eda2ff789830758a36a0
shivanggarg999 <999shivanggarg@gmail.com>
2024-04-02 23:40:43 +0530

    added helper, validation package joi, optimized routes defining

--- src/index.js
+++ src/index.js
@@ -4,27 +4,15 @@ import connectDB from "./db/index.js";
 import express from 'express';
 import cors from 'cors';
 
-
 connectDB();
 const app = express();
 app.use(cors());
 
-
 import CommanMiddleware from './middlewares/Comman.middleware.js';
 CommanMiddleware(app, express);
 
-
-import userRoutes from './routes/user.route.js';
-import categoryRoutes from './routes/category.route.js';
-import todoRoutes from "./routes/todo.route.js";
-
-
-app.use('/api/users', userRoutes);
-app.use('/api/category', categoryRoutes);
-app.use('/api/todo', todoRoutes);
-
-
-
+import Routes from './routes/index.route.js';
+Routes(app);
 
 
 app.listen(process.env.PORT, () => {
